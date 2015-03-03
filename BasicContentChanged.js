f1880107aafae81eb68331d08f59f4addb99df06
Jan Miksovsky <jan@quickui.org>
2015-03-03 13:55:59 -0800

    Detect changes in redistributed content.

--- BasicContentChanged.js
+++ BasicContentChanged.js
@@ -10,6 +10,19 @@
 var BasicContentChanged = {
 
   /**
+   * Return the host element for this element. If this element is not hosted by
+   * another element (it's not attached to anything, or is hosted by the
+   * top-level document), this returns null.
+   */
+  get host() {
+    for ( var parent = this.parentNode; parent != null; parent = parent.parentNode ) {
+      if ( parent.host ) {
+        return parent.host;
+      }
+    }
+  },
+
+  /**
    * If the element has a contentChanged handler, then wire up an observer 
    * that will invoke this handler whenever content changes.
    *
@@ -25,9 +38,7 @@ var BasicContentChanged = {
    * @method observeContentChanges
    */
   observeContentChanges: function(observeChanges) {
-    var observe = (observeChanges == null) ?
-      true :
-      observeChanges;
+    var observe = (observeChanges == null) ? true : observeChanges;
     if (observe) {
       // Start observing
       if (this.contentChanged) {
@@ -50,24 +61,24 @@ var BasicContentChanged = {
   // content includes a content element, in which case we'd need to monitor the
   // component's host for changes, too.
   _contentChanged: function() {
-    // var containsContentNode = (this.querySelector("content") != null);
-    // if (containsContentNode) {
-    //   // The element's new content contains <content> nodes, so from now on,
-    //   // we need to observe our host element, too.
-    //   // TODO: If this element's content changes again, disconnect any
-    //   // outstanding observers of that old content.
-    //   // TODO: If the host element *also* contains <content> nodes, we should
-    //   // watch those too. *sigh*
-    //   this._observeHostContentChanges();
-    // }
+    var containsContentNode = (this.querySelector('content') != null);
+    if (containsContentNode) {
+      // The element's new content contains <content> nodes, so from now on,
+      // we need to observe our host element, too.
+      // TODO: If this element's content changes again, disconnect any
+      // outstanding observers of that old content.
+      // TODO: If the host element *also* contains <content> nodes, we should
+      // watch those too. *sigh*
+      this._observeHostContentChanges();
+    }
     this.contentChanged();
   },
 
   // Wire up an observer for (light DOM) content change events on the given
   // node.
   _observeContentChanges: function(node, handler) {
-    this._contentChangeObserver = new MutationObserver(handler);
-    this._contentChangeObserver.observe(node, {
+    node._contentChangeObserver = new MutationObserver(handler);
+    node._contentChangeObserver.observe(node, {
       // attributes: true,
       characterData: true,
       childList: true,
