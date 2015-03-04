8d25b4710c7a3ef16cbce7ed3ac1bf5c61698d5d
Jan Miksovsky <jan@quickui.org>
2015-03-04 14:39:00 -0800

    window resize event should trigger resized handler.

--- test/BasicResizeHelpers.tests.js
+++ test/BasicResizeHelpers.tests.js
@@ -4,16 +4,42 @@ suite('BasicResizeHelpers', function() {
 
   var container = document.getElementById('container');
 
+  // Simulate a resize event.
+  var simulateResize = function() {
+    var event = new UIEvent('resize');
+    // event.bubbles = false;
+    // event.cancelBubble = false;
+    // event.cancelable = false;
+    // event.currentTarget = Window;
+    // event.defaultPrevented = false;
+    // event.path = Array[1];
+    // event.returnValue = true;
+    // event.srcElement = Window;
+    // event.target = Window;
+    // event.timeStamp = 1425508271138;
+    // event.type = "resize";
+    window.dispatchEvent(event);
+  };
+
   teardown(function() {
     container.innerHTML = '';
   });
 
   test('listenForResize should trigger initial call to resized handler', function() {
     var fixture = document.createElement('resize-test-element');
-    var spy = sinon.spy(fixture, 'resized');
     container.appendChild(fixture);
-    assert(spy.called);
-    fixture.resized.restore();
+    assert.equal(fixture.resizeCallCount, 1);
+  });
+
+  test('window resize event should trigger resized handler', function(done) {
+    var fixture = document.createElement('resize-test-element');
+    container.appendChild(fixture);
+    assert.equal(fixture.resizeCallCount, 1);
+    fixture.resizeCallHook = function() {
+      assert.equal(fixture.resizeCallCount, 2);
+      done();
+    }
+    simulateResize();
   });
 
 });
